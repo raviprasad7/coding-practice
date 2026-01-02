03e57986cc4e2b5d275129fb278936380208dd14
Ravi Prasad <ravi070994@gmail.com>
2026-01-02 19:03:13 +0530

    Add VSCode config

---
+++ 02-string/06-construct-string-with-repeat-element.ts
@@ -0,0 +1,6 @@
+function repeatLimitedString(s: string, repeatLimit: number): string {
+    
+};
+
+console.log(repeatLimitedString('cczazcc', 3)); // "zzcccac"
+console.log(repeatLimitedString('aababab', 2)); // "bbabaa"
