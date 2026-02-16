26308323d3f39fb0760ef8dcc0816aaa4d5c3122
Ravi Prasad <ravi070994@gmail.com>
2026-02-16 18:13:46 +0530

    More problems

---
+++ src/09-heap/01-k-closest-point-to-origin.ts
@@ -0,0 +1,13 @@
+function kClosest(points: number[][], k: number): number[][] {
+
+};
+
+{
+	const points = [[1,3],[-2,2]];
+	console.log(points, 1);
+}
+
+{
+	const points = [[3,3],[5,-1],[-2,4]];
+	console.log(points, 2);
+}
